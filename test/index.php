f16568956dd3cc850f985c5e2be650a430cab83b
m-cosoianu <302350382@student.rocmondriaan.nl>
2024-10-24 11:58:35 +0200

    the form checks if field is filled

--- test/index.php
+++ test/index.php
@@ -8,38 +8,67 @@
     <title>Document</title>
 </head>
 <body>
-<!--<form method="post" action="">-->
-<!--   <label for="n" class="form-label">Naam</label>-->
-<!--    <input type="text" class="form-control" id="n" name="naam"-->
-<!--           value="<php echo $inputs['name']"-->
-<!--    -->
+
+<?php
+//include 'dbconnect.php';
+
+const AMOUNT_REQUIRED = 'Aantal opgeven';
+const PRODUCT_REQUIRED = 'Product selecteren';
+
+$errors = [];
+$inputs = [];
+
+if(isset($_POST['send'])){
+
+    //amount
+    $amount=filter_input(INPUT_POST,'amount',FILTER_VALIDATE_INT);
+    if($amount===false)
+    {
+        $errors['amount']=AMOUNT_REQUIRED;
+    } else {
+        $inputs['amount']=$amount;
+    }
+
+    $product = filter_input(INPUT_POST, FILTER_VALIDATE_INT);
+    if($product==false){
+        $errors['product'] = PRODUCT_REQUIRED;
+    } else {
+        $inputs['product']=$product;
+    }
+}
+?>
+<div class="container">
 <h2>Stappelkorting</h2>
 <form method="post" action=" ">
     <label>Welke product wordt gekocht?</label> <br>
-    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"
-    <label>Handdoek</label>
-    <br>
-
-    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"
+    <input class="form-check-input" type="radio" name="product" value="1" id="flexRadioDeafult1"
+           <?php if (isset($inputs['product']) && $inputs['product']===1) {echo "checked";} ?>>
+    <label>Handdoek</label><br>
+    <input class="form-check-input" type="radio" name="product" id="flexRadioDeafult1" value="2"
+        <?php if (isset($inputs['product']) && $inputs['product']===2) {echo "checked";} ?>>
     <label>Broek</label>
     <br>
 
-    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"
+    <input class="form-check-input" type="radio" name="product" id="flexRadioDeafult1" value="3"
+        <?php if (isset($inputs['product']) && $inputs['product']===3) {echo "checked";} ?>>
     <label>Shirt</label>
-    <br>
-    <br>
+
+    <div class="form-text">
+        <?=$errors['product'] ?? '' ?>
+    </div>
+
+
  
     <label>Amount:</label>
-    <input type="text" name="name">
-    <br>
+    <input type="text" name="amount" value="<?php echo $inputs['amount'] ?? ''?>">
+    <div class="form-text">
+        <?=$errors['amount'] ?? '' ?>
+    </div>
+
+    <input type="submit" name="send" value="uitrekenen">
     <br>
 
-    <input type="submit" name="uitrekenen" value="uitrekenen">
-    <br>
-    <label>Vul alle velden in!</label>
-    <br>
-
-
+</div>
 </form>
 </body>
 </html>
