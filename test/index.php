b7e030934465140c70cccd7d96a8072311a926f7
m-cosoianu <302350382@student.rocmondriaan.nl>
2024-10-24 10:07:34 +0200

    html code form

---
+++ test/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!--<form method="post" action="">-->
+<!--   <label for="n" class="form-label">Naam</label>-->
+<!--    <input type="text" class="form-control" id="n" name="naam"-->
+<!--           value="<php echo $inputs['name']"-->
+<!--    -->
+<h2>Stappelkorting</h2>
+<form method="post" action=" ">
+    <label>Welke product wordt gekocht?</label> <br>
+    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"
+    <label>Handdoek</label>
+    <br>
+
+    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"
+    <label>Broek</label>
+    <br>
+
+    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"
+    <label>Shirt</label>
+    <br>
+    <br>
+ 
+    <label>Amount:</label>
+    <input type="text" name="name">
+    <br>
+    <br>
+
+    <input type="submit" name="uitrekenen" value="uitrekenen">
+    <br>
+    <label>Vul alle velden in!</label>
+    <br>
+
+
+</form>
+</body>
+</html>
