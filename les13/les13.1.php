c685f83627a4c3117df79dbe3aadcf05c150587c
m-cosoianu <302350382@student.rocmondriaan.nl>
2024-11-28 10:08:12 +0100

    les 13

--- les13/les13.1.php
+++ les13/les13.1.php
@@ -1,35 +1,33 @@
-<!---->
 <?php
-
-
 try {
-    $db = new PDO("mysql:host=localhost;dbname=fietsenmaker", "root", "");
+    $db = new PDO("mysql:host=localhost;dbname=fietsenmaker","root", "");
     $query = $db->prepare("select * FROM fietsen");
     $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     //var_dump($result);
-    foreach ($result as $data) {
-        echo "<a href='detail.php?id=" . $data['id'] . "'>";
-        echo $data["merk"] . " " . $data["type"];
-        echo "</a>";
-        echo "<br>";
+    echo "<table>";
+    foreach($result as $data) {
+        echo "<tr>";
+        echo "<td>". $data["merk"] . "</td> ";
+        echo "<td>" . $data["type"] . "</td> ";
+        echo "<td>" . $data["prijs"] . "</td>"; echo "</tr>";
     }
-} catch (PDOException $e) {
+    echo "</table>";
+} catch(PDOException $e) {
     die("Error!: " . $e->getMessage());
 }
 //echo "<td>$euro; " . number_format($data["prijs"],2,",",".") . "<td>";
 ?>
 
-<style type="text/css">
-    table {
+<style  type="text/css">
+    table{
         border-collaps: collapse;
         border: 1px solid black;
     }
-
-    td {
+    td{
         border: 1px solid black;
         widtch: 100px;
     }
 </style>
 
-
+?>
